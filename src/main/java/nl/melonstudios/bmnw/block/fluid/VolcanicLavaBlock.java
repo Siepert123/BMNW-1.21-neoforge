911d8bf386ad988af78b912e6956aeff6a072e04
Siepert123 <createlegacy69@gmail.com>
2025-07-06 14:19:57 +0200

    small change

--- src/main/java/nl/melonstudios/bmnw/block/fluid/VolcanicLavaBlock.java
+++ src/main/java/nl/melonstudios/bmnw/block/fluid/VolcanicLavaBlock.java
@@ -1,12 +1,15 @@
 package nl.melonstudios.bmnw.block.fluid;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LiquidBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.FlowingFluid;
+import net.neoforged.neoforge.common.Tags;
 
 public class VolcanicLavaBlock extends LiquidBlock {
     public VolcanicLavaBlock(FlowingFluid fluid, Properties properties) {
@@ -22,5 +25,17 @@ public class VolcanicLavaBlock extends LiquidBlock {
     protected void onRemove(BlockState state, Level level, BlockPos pos, BlockState newState, boolean movedByPiston) {
         if (state.is(newState.getBlock()) || movedByPiston) return;
         if (!newState.getFluidState().isEmpty()) level.setBlock(pos, Blocks.BASALT.defaultBlockState(), 3);
+    }
+
+    @Override
+    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
+        super.neighborChanged(state, level, pos, block, fromPos, isMoving);
+        for (Direction d : Direction.values()) {
+            if (d == Direction.DOWN) continue;
+            if (level.getBlockState(pos.relative(d)).getFluidState().is(Tags.Fluids.WATER)) {
+                level.setBlock(pos, Blocks.BASALT.defaultBlockState(), 3);
+                return;
+            }
+        }
     }
 }
